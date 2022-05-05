27b084607ea859910583b844049adcbd487ffa2d
sdecri <simone.decristofaro85@gmail.com>
2022-05-05 14:35:04 +0200

    First draft

---
+++ SwashbucklerEmailAttribute/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+using SwashbucklerEmailAttribute.Models;
+
+namespace SwashbucklerEmailAttribute.Controllers
+{
+  [ApiController]
+  [Route("email-service")]
+  public class EmailController : ControllerBase
+  {
+
+    const long MAX_ATTACHMENT_SIZE = 5242880;
+
+    private readonly ILogger<EmailController> _logger;
+
+
+    [HttpPost("send-email")]
+    public IActionResult SendEmailAsync([FromForm] SendEmailRequest req)
+    {
+
+      _logger.LogInformation(req.ToJson());
+      return Ok();
+    }
+
+
+  }
+}
---
+++ SwashbucklerEmailAttribute/Modeles/SendEmailRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwashbucklerEmailAttribute.Models
+{
+
+  public interface IIdentifiers
+  {
+    public string[] recipients { get; set; }
+    public string from { get; set; }
+  }
+
+  public class SendEmailRequest : IIdentifiers
+  {
+    protected const int MAX_RECIPIENTS = 10000;
+
+    [Required]
+    [MaxLength(MAX_RECIPIENTS)]
+    [EmailAddress]
+    public string[] recipients { get; set; }
+
+    [EmailAddress]
+    public string from { get; set; }
+
+  }
+
+}
